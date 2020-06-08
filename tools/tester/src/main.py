5a6b7ebacad1f988a4409e2de500b25eda1d3781
computations <bwbettisworth@gmail.com>
2020-06-08 15:07:26 +0200

    Improves the usability of the testing script

--- tools/tester/src/main.py
+++ tools/tester/src/main.py
@@ -2,12 +2,25 @@
 
 import argparse
 import tester
+import tempfile
+import os
+
+SOURCE_DIR = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
+DEFAULT_PROGRAM = os.path.abspath(os.path.join(SOURCE_DIR,
+    "../../../bin/lagrange"))
+DEFAULT_ARCHIVE = os.path.abspath(os.path.join(SOURCE_DIR,
+    "../archives/regression_tests_short_created_2020-06-05.tar.gz"))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--prefix', type=str, required=True)
-    parser.add_argument('--archive', type=str, required=True)
-    parser.add_argument('--program', type=str, required=True)
+    parser.add_argument('--prefix', type=str)
+    parser.add_argument('--archive', type=str, default=DEFAULT_ARCHIVE)
+    parser.add_argument('--program', type=str, default=DEFAULT_PROGRAM)
     args = parser.parse_args()
+
+    if args.prefix is None:
+        tempdir = tempfile.TemporaryDirectory()
+        print("Using the tempdir:", tempdir.name)
+        args.prefix = tempdir.name
 
     tester.run(args.prefix, args.archive, args.program)
