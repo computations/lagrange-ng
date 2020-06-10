70bb5150a6eedc04fb676e6dd2fb6e2577906fcb
computations <bwbettisworth@gmail.com>
2020-06-10 17:30:40 +0200

    Rename and remove dead code from OptimizeBioGeo

--- src/OptimizeBioGeo.h
+++ src/OptimizeBioGeo.h
@@ -18,11 +18,11 @@ using namespace std;
 
 class OptimizeBioGeo {
 private:
-  BioGeoTree *tree;
-  RateModel *rm;
-  int maxiterations;
-  double stoppingprecision;
-  bool marginal;
+  BioGeoTree *_tree;
+  RateModel *_rate_model;
+  const int _max_iterations = 100;
+  const double _abs_tol = 0.0001;
+  bool _marginal;
   double
   GetLikelihoodWithOptimizedDispersalExtinction(const gsl_vector *variables);
   static double
