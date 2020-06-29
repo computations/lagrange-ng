acd1e0c004b42f068e38067a48912f011c6423fd
computations <bwbettisworth@gmail.com>
2020-06-29 17:39:54 +0200

    When generating profile names, use a more descriptive name if available
    Now we use the git tag instead of the hash when there exists a tag.

--- tools/profiler/src/main.py
+++ tools/profiler/src/main.py
@@ -8,6 +8,14 @@ import datetime
 import util
 import rich
 
+def git_describe(repo):
+    description = repo.head.commit.hexsha[0:7]
+    for t in repo.tags:
+        if t.commit == repo.head.commit:
+            description = t.name
+            break
+    return description
+
 if __name__ == "__main__":
 
     SOURCE_DIR = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
@@ -39,7 +47,7 @@ if __name__ == "__main__":
     if args.prefix is None:
         repo = git.Repo(os.path.abspath(os.path.join(SOURCE_DIR, '../../../')))
         commit_string = datetime.datetime.now().strftime('%Y-%m-%d') + "_"\
-                + repo.head.commit.hexsha[0:7] + "_"\
+                + git_describe(repo) + "_"\
                 + util.make_random_nonce()
         rich.print("Placing results in [red bold]{}[/red bold]".format(commit_string))
 
