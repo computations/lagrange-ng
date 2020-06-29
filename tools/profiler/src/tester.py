ddf296f442f557efdafd60736177ce9e4ee98226
computations <bwbettisworth@gmail.com>
2020-06-29 18:04:24 +0200

    Migrated profiling and testing to their own repo

--- tools/profiler/src/tester.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--binary", type=string, required=True)
-parser.add_argument("--datapath", type=string, required=True)
-args = parser.parse_args()
