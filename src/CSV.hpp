af1692b3b76b5e6bfd0d4c9cea52103c25c7bbca
computations <bwbettisworth@gmail.com>
2025-09-21 13:08:46 +0300

    fix: Fix the mac build

--- src/CSV.hpp
+++ src/CSV.hpp
@@ -4,11 +4,15 @@
 #include <concepts>
 #include <filesystem>
 #include <fstream>
+#include <numeric>
 #include <ranges>
 #include <sstream>
 #include <string>
 #include <string_view>
 #include <vector>
+
+// #ifndef __cpp_lib_ranges_join_with
+// #endif
 
 template <std::ranges::range R>
 auto make_csv_row(const R& entries) -> auto
@@ -16,17 +20,36 @@ auto make_csv_row(const R& entries) -> auto
            || std::convertible_to<std::ranges::range_value_t<R>,
                                   std::string_view>
 {
+#ifdef __cpp_lib_ranges_join_with
   return entries | std::ranges::views::join_with(',');
+#else
+  using namespace std::string_literals;
+  return std::views::zip(entries, std::views::repeat(","s))
+         | std::views::transform([](auto t) -> std::vector<std::string> {
+             return {std::string(std::get<0>(t)), std::get<1>(t)};
+           })
+         | std::views::join | std::views::take(2 * entries.size() - 1);
+
+#endif
 }
 
 template <std::ranges::range R>
 auto make_csv_row(const R& entries) -> auto
   requires std::floating_point<std::ranges::range_value_t<R>>
 {
+#ifdef __cpp_lib_ranges_join_with
   return entries | std::views::transform([](const auto& val) -> std::string {
            return std::to_string(val);
          })
          | std::ranges::views::join_with(',');
+#else
+  using namespace std::string_literals;
+  return std::views::zip(entries, std::views::repeat(","s))
+         | std::views::transform([](auto t) -> std::vector<std::string> {
+             return {std::to_string(std::get<0>(t)), std::get<1>(t)};
+           })
+         | std::views::join | std::views::take(2 * entries.size() - 1);
+#endif
 }
 
 template <std::ranges::range R>
